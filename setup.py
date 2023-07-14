f747c876368cff76874000f1746f544a25ee889e
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-07-14 23:27:06 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='classification',
+    version='0.1',
+    description='Classification package',
+    long_description=readme,
+    author='Ivan Pelizon',
+    author_email='ivan.pelizon@gmail.com',
+    url='',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
+install_requires = [
+    'python_version>=3.8',
+    'keras-cv>=2.13',
+    'tensorflow>=2.13'
+]
---
+++ src/classification/__init__.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+from tensorflow import keras
+import keras_cv
+import tensorflow_datasets as tfds
