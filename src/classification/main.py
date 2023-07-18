3e982d9c935b1998ab11671e85fdb1c205e27902
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-07-18 12:19:26 +0200

    Refactor, removed duplicated code

--- src/classification/main.py
+++ src/classification/main.py
@@ -3,6 +3,3 @@ from classification.DatasetBuilder import DatasetBuilder
 builder = DatasetBuilder('/mnt/DATA/tesi/dataset/dataset_classification/pallacanestro_trieste')
 builder.configure_datasets_for_performance()
 train_dataset, validation_dataset = builder.build()
-
-
-
