d7ed3a905d991ad2fa09017c71d0b3e471250bb1
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-08-02 01:57:02 +0200

    Importing segmentation data same way as classification data

--- classification/dataset_builders.py
+++ classification/dataset_builders.py
@@ -1,15 +1,14 @@
 import pathlib
 import os.path
-import random
 from typing import Any
 
 import tensorflow as tf
 import numpy as np
 
 
-def decode_image(image_data, image_width: int = 50, image_height: int = 50):
-    image = tf.io.decode_png(image_data, channels=3)
-    return tf.image.resize(image, [image_width, image_height])
+def decode_image(image_data, image_width: int = 50, image_height: int = 50, channels: int = 3):
+    image = tf.io.decode_png(image_data, channels=channels)
+    return tf.image.resize(image, [image_height, image_width])
 
 
 class ClassificationDatasetBuilder:
@@ -61,12 +60,12 @@ class ClassificationDatasetBuilder:
     def validation_dataset(self) -> tf.data.Dataset:
         return self.__validation_dataset
 
-    def __get_label(self, file_path: str) -> int:
+    def __get_label(self, file_path: tf.Tensor) -> int:
         parts = tf.strings.split(file_path, os.path.sep)
         one_hot = parts[-2] == self.__class_names
         return tf.argmax(one_hot)
 
-    def __get_image_label_pair_from_path(self, file_path: str) -> (Any, int):
+    def __get_image_label_pair_from_path(self, file_path: tf.Tensor) -> (Any, int):
         label = self.__get_label(file_path)
         image_data = tf.io.read_file(file_path)
         image = decode_image(image_data)
@@ -101,48 +100,53 @@ class SegmentationDatasetBuilder:
             if filename.endswith('.png')
         ]
         number_of_validation_samples = int(len(input_image_paths) * validation_percentage)
+        print(len(input_image_paths), 'frames, with', len(mask_image_paths), 'corresponding ground truth masks')
+        print(number_of_validation_samples, 'samples will be set aside as validation data')
+
+        if len(input_image_paths) != len(mask_image_paths):
+            raise ValueError('The number of frames is different than the number of ground truth masks, aborting')
 
         input_image_paths.sort()
-        input_mask_path_pairs = []
-        for mask_path in mask_image_paths:
-            frame_number = int(mask_path.split('_')[-1].split('.')[-2])
-            input_mask_path_pairs.append((input_image_paths[frame_number - 1], mask_path))
+        mask_image_paths.sort(key=lambda file_path: file_path.split('_')[-1].split('.')[-2])
+        samples_dataset = tf.data.Dataset.from_tensor_slices(tf.constant(input_image_paths))
+        masks_dataset = tf.data.Dataset.from_tensor_slices(tf.constant(mask_image_paths))
 
-        random.Random(2023).shuffle(input_mask_path_pairs)
-        self.__train_input_mask_path_pairs = input_mask_path_pairs[:-number_of_validation_samples]
-        self.__validation_input_mask_path_pairs = input_mask_path_pairs[-number_of_validation_samples:]
+        samples_dataset = samples_dataset.map(
+            self.__get_frame_from_path,
+            num_parallel_calls=tf.data.AUTOTUNE
+        )
+        masks_dataset = masks_dataset.map(
+            self.__get_mask_from_path,
+            num_parallel_calls=tf.data.AUTOTUNE
+        )
+        dataset = tf.data.Dataset.zip((samples_dataset, masks_dataset))
+        dataset.shuffle(len(mask_image_paths), reshuffle_each_iteration=False)
+        self.__train_dataset = dataset.skip(number_of_validation_samples)
+        self.__validation_dataset = dataset.take(number_of_validation_samples)
+        print(tf.data.experimental.cardinality(self.__train_dataset).numpy(), 'frames in training dataset')
+        print(tf.data.experimental.cardinality(self.__validation_dataset).numpy(), 'frames in validation dataset')
+
+    @staticmethod
+    def __get_frame_from_path(filepath: tf.Tensor):
+        image_data = tf.io.read_file(filepath)
+        image = decode_image(image_data, image_width=1024, image_height=512)
+        return image
+
+    @staticmethod
+    def __get_mask_from_path(filepath: tf.Tensor):
+        image_data = tf.io.read_file(filepath)
+        image = decode_image(image_data, image_width=1024, image_height=512, channels=1)
+        return image
 
     @property
-    def train_input_mask_path_pairs(self) -> [(str, str)]:
-        return self.__train_input_mask_path_pairs
+    def train_dataset(self) -> tf.data.Dataset:
+        return self.__train_dataset
 
     @property
-    def validation_input_mask_path_pairs(self) -> [(str, str)]:
-        return self.__validation_input_mask_path_pairs
+    def validation_dataset(self) -> tf.data.Dataset:
+        return self.__validation_dataset
 
-
-class Basketballs(tf.keras.utils.Sequence):
-    def __init__(self,
-                 input_mask_path_pairs: [(str, str)],
-                 batch_size: int = 1000,
-                 image_size: (int, int) = (1080, 1920)):
-        self.__batch_size = batch_size
-        self.__image_size = image_size
-        self.__input_mask_path_pairs = input_mask_path_pairs
-
-    def __getitem__(self, index) -> ([], []):
-        i = index * self.__batch_size
-        batch_input_mask_path_pairs = self.__input_mask_path_pairs[i: i + self.__batch_size]
-        x = np.zeros((self.__batch_size,) + self.__image_size + (3,), dtype='float32')
-        y = np.zeros((self.__batch_size,) + self.__image_size + (1,), dtype='uint8')
-        for j, (input_path, mask_path) in enumerate(batch_input_mask_path_pairs):
-            frame_image = tf.keras.utils.load_img(input_path, target_size=self.__image_size)
-            rescale = tf.keras.layers.Rescaling(1. / 255)
-            x[j] = rescale(frame_image)
-            mask_image = tf.keras.utils.load_img(mask_path, target_size=self.__image_size, color_mode='grayscale')
-            y[j] = np.expand_dims(rescale(mask_image), 2)
-            y[j] -= 1
-        return x, y
-
-    def __len__(self):
-        return len(self.__input_mask_path_pairs) // self.__batch_size
+    @property
+    def number_of_samples(self) -> int:
+        return tf.data.experimental.cardinality(self.__train_dataset).numpy() + \
+               tf.data.experimental.cardinality(self.__validation_dataset).numpy()
