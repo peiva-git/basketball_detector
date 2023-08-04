a25ed419c5b5bc39b62c1d3ace27b7cca6906f13
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-08-04 15:27:53 +0200

    Using model from package

--- classification/models/models.py
+++ classification/models/models.py
@@ -1,7 +1,7 @@
 import os
 import tensorflow as tf
 import keras_cv
-from tensorflow_examples.models.pix2pix import pix2pix
+from keras_segmentation.models.pspnet import pspnet_50
 
 
 class Classifier:
@@ -72,52 +72,9 @@ class Classifier:
 
 class Segmentation:
     def __init__(self, output_channels: int = 2, image_size: (int, int) = (1080, 1920)):
-        base_model = tf.keras.applications.MobileNetV2(input_shape=image_size + (3,), include_top=False)
 
-        # Use the activations of these layers
-        layer_names = [
-            'block_1_expand_relu',  # 64x64
-            'block_3_expand_relu',  # 32x32
-            'block_6_expand_relu',  # 16x16
-            'block_13_expand_relu',  # 8x8
-            'block_16_project',  # 4x4
-        ]
-        base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
-
-        # Create the feature extraction model
-        down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
-
-        down_stack.trainable = False
-
-        up_stack = [
-            pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-            pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-            pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-            pix2pix.upsample(64, 3),  # 32x32 -> 64x64
-        ]
-
-        inputs = tf.keras.layers.Input(shape=[128, 128, 3])
-
-        # Downsampling through the model
-        skips = down_stack(inputs)
-        x = skips[-1]
-        skips = reversed(skips[:-1])
-
-        # Upsampling and establishing the skip connections
-        for up, skip in zip(up_stack, skips):
-            x = up(x)
-            concat = tf.keras.layers.Concatenate()
-            x = concat([x, skip])
-
-        # This is the last layer of the model
-        last = tf.keras.layers.Conv2DTranspose(
-            filters=output_channels, kernel_size=3, strides=2,
-            padding='same')  # 64x64 -> 128x128
-
-        x = last(x)
-
-        self.__model = tf.keras.Model(inputs=inputs, outputs=x)
-        self.__model_name = 'segmenter'
+        self.__model = pspnet_50(n_classes=output_channels, input_height=image_size[0], input_width=image_size[1])
+        self.__model_name = 'pspnet'
         self.__model.summary()
 
     @property
