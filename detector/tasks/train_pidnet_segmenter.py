17730e0c6b424baf1f09b60166d42257f9b16208
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-08-24 18:19:04 +0200

    Inverted saving order, modified momentum parameter

--- detector/tasks/train_pidnet_segmenter.py
+++ detector/tasks/train_pidnet_segmenter.py
@@ -9,13 +9,13 @@ import tensorflow as tf
 if __name__ == '__main__':
     builder = SegmentationDatasetBuilder('/home/ubuntu/segmentation_dataset/pallacanestro_trieste')
     # builder.augment_train_dataset()
-    builder.configure_datasets_for_performance(shuffle_buffer_size=50, input_batch_size=12)
+    builder.configure_datasets_for_performance(shuffle_buffer_size=24, input_batch_size=12)
     train_dataset, validation_dataset = builder.train_dataset, builder.validation_dataset
 
     segmenter = PIDNetSmall(input_shape=(1024, 2048, 3), number_of_classes=2)
     segmenter.model.compile(
         loss=tf.keras.losses.BinaryCrossentropy(),
-        optimizer=tf.keras.optimizers.SGD(momentum=0.0005, learning_rate=0.01),
+        optimizer=tf.keras.optimizers.SGD(momentum=0.05, learning_rate=0.01),
         metrics=['accuracy', tf.keras.metrics.IoU(num_classes=2, target_class_ids=[1])]
     )
     segmenter.model.summary()
@@ -25,6 +25,6 @@ if __name__ == '__main__':
         epochs=100,
         callbacks=get_segmentation_model_callbacks(segmenter.model_name, early_stop_patience=15, reduce_lr_patience=3)
     )
-    segmenter.model.save(filepath=os.path.join('out', 'models', 'HDF5', segmenter.model_name + '.h5'), save_format='h5')
     segmenter.model.save(filepath=os.path.join('out', 'models', 'Keras_v3', segmenter.model_name + '.keras'))
+    segmenter.model.save(filepath=os.path.join('out', 'models', 'HDF5', segmenter.model_name + '.h5'), save_format='h5')
 
