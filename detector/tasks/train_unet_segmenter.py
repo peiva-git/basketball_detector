7d250fefa74ca8d6ab23a6a57f179ab8a6a66d37
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-01 11:52:39 +0200

    Switching to other class for IoU metric

--- detector/tasks/train_unet_segmenter.py
+++ detector/tasks/train_unet_segmenter.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     segmenter.model.compile(
         loss=tf.keras.losses.BinaryCrossentropy(),
         optimizer=tf.keras.optimizers.SGD(momentum=0.9, learning_rate=0.01),
-        metrics=['accuracy', tf.keras.metrics.IoU(num_classes=2, target_class_ids=[1])]
+        metrics=['accuracy', tf.keras.metrics.IoU(num_classes=2, target_class_ids=[0])]
     )
     segmenter.model.fit(
         train_dataset,
