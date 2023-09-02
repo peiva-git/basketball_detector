07593ec68627d435d84bbcd81a9453fa85d5d9f9
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-02 16:13:14 +0200

    Refactored tests

--- tests/prediction_tests.py
+++ tests/prediction_tests.py
@@ -1,22 +1,25 @@
 import unittest
 
+from parameterized import parameterized
 from detector.tasks.predict_ball_locations import patch_indexes_from_coordinates
 
 
 class PredictionTestCase(unittest.TestCase):
 
-    def test_patch_indexes_from_coordinates(self):
-        self.assertListEqual(patch_indexes_from_coordinates(0, 0, 512, 1024, stride=10, window_size=50), [0],
-                             'Left upper corner pixel should be embedded only in the first patch')
-        self.assertListEqual(patch_indexes_from_coordinates(0, 10, 512, 1024, stride=10, window_size=50), [0, 1])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 20, 512, 1024, stride=10, window_size=50), [0, 1, 2])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 30, 512, 1024, stride=10, window_size=50), [0, 1, 2, 3])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 40, 512, 1024, stride=10, window_size=50), [0, 1, 2, 3, 4])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 50, 512, 1024, stride=10, window_size=50), [1, 2, 3, 4, 5])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 60, 512, 1024, stride=10, window_size=50), [2, 3, 4, 5, 6])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 980, 512, 1024, stride=10, window_size=50), [94, 95, 96])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 990, 512, 1024, stride=10, window_size=50), [95, 96])
-        self.assertListEqual(patch_indexes_from_coordinates(0, 1000, 512, 1024, stride=10, window_size=50), [96])
+    @parameterized.expand([
+        (0, 0, [0]),
+        (0, 10, [0, 1]),
+        (0, 20, [0, 1, 2]),
+        (0, 30, [0, 1, 2, 3]),
+        (0, 40, [0, 1, 2, 3, 4]),
+        (0, 50, [1, 2, 3, 4, 5]),
+        (0, 60, [2, 3, 4, 5, 6]),
+        (0, 980, [94, 95, 96]),
+        (0, 990, [95, 96]),
+        (0, 1000, [96])
+    ])
+    def test_patch_indexes_from_coordinates(self, row: int, column: int, expected: list[int]):
+        self.assertListEqual(patch_indexes_from_coordinates(row, column, 512, 1024, stride=10, window_size=50), expected)
 
 
 
