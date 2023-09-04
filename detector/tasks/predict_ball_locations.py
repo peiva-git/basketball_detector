476be5a7c7888a48c1209874aa61a61e1700de83
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-04 16:01:58 +0200

    Added tests for right columns middle rows

--- detector/tasks/predict_ball_locations.py
+++ detector/tasks/predict_ball_locations.py
@@ -156,6 +156,11 @@ def __get_indexes(row: int, column: int, number_of_height_windows: int, number_o
             for mult in range(int(row / stride) + 1):
                 result.extend(sorted([i for i in range(number_of_width_windows * (mult + 1) - 1, int(column / stride) - int(window_size / stride) + number_of_width_windows * mult, -1)]))
             return result
+        if stride * (int(window_size / stride)) <= row < stride * (number_of_height_windows - int(window_size / stride)) + window_size:
+            result = []
+            for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
+                result.extend(sorted([i for i in range(number_of_width_windows * (mult + 1) - 1, int(column / stride) - int(window_size / stride) + number_of_width_windows * mult, -1)]))
+            return result
 
 
 def map_pixels_to_patch_indexes(patch_indexes_by_pixel, patches_with_positions, window_size: int):
