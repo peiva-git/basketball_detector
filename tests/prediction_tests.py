62f82d0a600481f45ad5cbe73d5f58004af57b2e
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-04 11:30:05 +0200

    Improved test coverage

--- tests/prediction_tests.py
+++ tests/prediction_tests.py
@@ -33,6 +33,10 @@ class PredictionTestCase(unittest.TestCase):
         (20, 10, [0, 1, 97, 98, 194, 195]),
         (20, 20, [0, 1, 2, 97, 98, 99, 194, 195, 196]),
         (20, 30, [0, 1, 2, 3, 97, 98, 99, 100, 194, 195, 196, 197]),
+        (20, 40, [0, 1, 2, 3, 4, 97, 98, 99, 100, 101, 194, 195, 196, 197, 198]),
+        (20, 980, [94, 95, 96, 191, 192, 193, 288, 289, 290]),
+        (20, 990, [95, 96, 192, 193, 289, 290]),
+        (20, 1000, [96, 193, 290])
     ])
     def test_patch_indexes_from_coordinates(self, row: int, column: int, expected: list[int]):
         self.assertListEqual(patch_indexes_from_coordinates(row, column, 512, 1024, stride=10, window_size=50), expected)
