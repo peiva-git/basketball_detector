dcb80e32b882a1a4e8cb64cb7e93cc3c3a199b64
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-05 16:00:59 +0200

    Refactor

--- detector/tasks/predict_ball_locations.py
+++ detector/tasks/predict_ball_locations.py
@@ -124,23 +124,38 @@ def patch_indexes_from_coordinates(row: int, column: int,
     return __get_indexes(row, column, number_of_height_windows, number_of_width_windows, window_size, stride)
 
 
+def __get_indexes_for_middle_rows(row: int, column: int,
+                                            number_of_width_windows: int,
+                                            window_size: int = 50, stride: int = 10):
+    result = []
+    for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
+        result.extend([i for i in range(number_of_width_windows * mult + int(column / window_size),
+                                        number_of_width_windows * mult + int(column / stride) + 1)])
+    return result
+
+
+def __get_indexes_for_lower_rows(row: int, column: int,
+                                 number_of_width_windows: int,
+                                 window_size: int = 50, stride: int = 10):
+    result = []
+    for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
+        result.extend([i for i in range(number_of_width_windows * (mult - 1) + int(column / window_size),
+                                        number_of_width_windows * (mult - 1) + int(column / stride) + 1)])
+    return result
+
+
 def __get_indexes(row: int, column: int, number_of_height_windows: int, number_of_width_windows: int, window_size: int = 50, stride: int = 10):
-    if column < stride * (int(window_size / stride) - 1):
-        if row < stride * (int(window_size / stride)):  # stride * 5
+    # comments assuming default values
+    if column < stride * (int(window_size / stride) - 1):  # columns < 40 (less than 5 indexes per row)
+        if row < stride * (int(window_size / stride)):  # rows < 50
             result = []
             for mult in range(int(row / stride) + 1):
                 result.extend([i for i in range(number_of_width_windows * mult, number_of_width_windows * mult + int(column / stride) + 1)])
             return result
         if stride * (int(window_size / stride)) <= row < stride * (number_of_height_windows - int(window_size / stride)) + window_size:
-            result = []
-            for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
-                result.extend([i for i in range(number_of_width_windows * mult + int(column / window_size), number_of_width_windows * mult + int(column / stride) + 1)])
-            return result
+            return __get_indexes_for_middle_rows(row, column, number_of_width_windows, window_size, stride)
         if stride * (number_of_height_windows - int(window_size / stride)) + window_size <= row < stride * (number_of_height_windows) + window_size:
-            result = []
-            for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
-                result.extend([i for i in range(number_of_width_windows * (mult - 1) + int(column / window_size), number_of_width_windows * (mult - 1) + int(column / stride) + 1)])
-            return result
+            return __get_indexes_for_lower_rows(row, column, number_of_width_windows, window_size, stride)
 
     if stride * (int(window_size / stride) - 1) <= column < stride * (number_of_width_windows - int(window_size / stride)) + window_size:
         if row < stride * (int(window_size / stride)):
@@ -149,15 +164,9 @@ def __get_indexes(row: int, column: int, number_of_height_windows: int, number_o
                 result.extend([i for i in range(int(column / stride) - int(window_size / stride) + 1 + number_of_width_windows * mult, int(column / stride) + 1 + number_of_width_windows * mult)])
             return result
         if stride * (int(window_size / stride)) <= row < stride * (number_of_height_windows - int(window_size / stride)) + window_size:
-            result = []
-            for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
-                result.extend([i for i in range(number_of_width_windows * mult + int(column / window_size), number_of_width_windows * mult + int(column / stride) + 1)])
-            return result
+            return __get_indexes_for_middle_rows(row, column, number_of_width_windows, window_size, stride)
         if stride * (number_of_height_windows - int(window_size / stride)) + window_size <= row < stride * (number_of_height_windows) + window_size:
-            result = []
-            for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
-                result.extend([i for i in range(number_of_width_windows * (mult - 1) + int(column / window_size), number_of_width_windows * (mult - 1) + int(column / stride) + 1)])
-            return result
+            return __get_indexes_for_lower_rows(row, column, number_of_width_windows, window_size, stride)
     if stride * (number_of_width_windows - int(window_size / stride)) + window_size <= column < stride * (number_of_width_windows) + window_size:
         if row < stride * (int(window_size / stride)):
             result = []
