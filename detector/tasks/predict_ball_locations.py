f7eff6a711c1d982153d873f622bfedc5613aa9c
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-07 13:13:41 +0200

    Added test, reverted refactoring

--- detector/tasks/predict_ball_locations.py
+++ detector/tasks/predict_ball_locations.py
@@ -199,9 +199,25 @@ def __get_indexes(row: int, column: int,
                           int(column / stride) + 1 + number_of_width_windows * mult)
                 ])
             return result
-        return __get_indexes_for_middle_or_lower_rows(row, column,
-                                                      number_of_height_windows, number_of_width_windows,
-                                                      window_size, stride)
+        if stride * (int(window_size / stride)) \
+                <= row < stride * (number_of_height_windows - int(window_size / stride)) + window_size:
+            result = []
+            for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
+                result.extend([i for i in range(number_of_width_windows * mult + int(column / window_size),
+                                                number_of_width_windows * mult + int(column / stride) + 1)])
+            return result
+        if stride * (number_of_height_windows - int(window_size / stride)) + window_size \
+                <= row < stride * number_of_height_windows + window_size - stride:
+            result = []
+            for mult in range(int((row - window_size) / stride) + 1, int(row / stride) + 1):
+                result.extend([i for i in range(number_of_width_windows * (mult - 1) + int(column / window_size),
+                                                number_of_width_windows * (mult - 1) + int(column / stride) + 1)])
+            return result
+        if stride * number_of_height_windows + window_size - stride \
+                <= row < stride * number_of_height_windows + window_size:
+            return
+        else:
+            return []
 
     if stride * (number_of_width_windows - int(window_size / stride)) + window_size \
             <= column < stride * number_of_width_windows + window_size:
