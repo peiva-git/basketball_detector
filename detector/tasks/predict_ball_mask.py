6b5f519ba5b98d6d892f29ab4e4a035db8e2f6e2
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-12 16:01:58 +0200

    Reverted to local experiments

--- detector/tasks/predict_ball_mask.py
+++ detector/tasks/predict_ball_mask.py
@@ -12,13 +12,13 @@ TEST_DATA_DIR = '/mnt/DATA/tesi/dataset/dataset_paddleseg/images/'
 
 if __name__ == '__main__':
     session = ort.InferenceSession(
-        #'/home/peiva/ppliteSeg/inference_model.onnx',
-        '/home/ubuntu/PaddleSeg/output/inference_model.onnx',
+        '/home/peiva/ppliteSeg/inference_model.onnx',
+        #'/home/ubuntu/PaddleSeg/output/inference_model.onnx',
         providers=['CUDAExecutionProvider', 'CPUExecutionProvider']
     )
     stream = CamGear(
-        #source='/mnt/DATA/tesi/dataset/dataset_youtube/pallacanestro_trieste/stagione_2019-20_legabasket/pallacanestro_trieste-dolomiti_energia_trentino/final_cut.mp4'
-        source='/home/ubuntu/final_cut.mp4'
+        source='/mnt/DATA/tesi/dataset/dataset_youtube/pallacanestro_trieste/stagione_2019-20_legabasket/pallacanestro_trieste-dolomiti_energia_trentino/final_cut.mp4'
+        #source='/home/ubuntu/final_cut.mp4'
     ).start()
     index = 1
     while True:
@@ -32,16 +32,15 @@ if __name__ == '__main__':
         print(f'Input name: {input_name}')
 
         output = session.run([], {input_name: batch.astype(np.float32)})
-        mask_batch = np.asarray(output, dtype=np.uint8)
-        mask_channels_first = np.squeeze(mask_batch, axis=0)
+        mask_channels_first = np.asarray(output[0], dtype=np.uint8)
         mask = np.moveaxis(mask_channels_first, source=0, destination=-1).squeeze()
         # mask_rescaled = mask * 255
         color = np.array([0, 255, 0], dtype=np.uint8)
         masked_image = np.where(mask[..., None], color, frame_resized)
         frame_with_overlay = cv.addWeighted(frame_resized, 0.8, masked_image, 0.2, 0)
-        # cv.imshow('Output', frame_with_overlay)
+        cv.imshow('Output', frame_with_overlay)
         #cv.imwrite(f'/home/peiva/experiments/test_videos/overlay{index}.png', frame_with_overlay)
-        cv.imwrite(f'/home/ubuntu/results/overlay{index}.png', frame_with_overlay)
+        #cv.imwrite(f'/home/ubuntu/results/overlay{index}.png', frame_with_overlay)
         index += 1
 
         key = cv.waitKey(1) & 0xFF
