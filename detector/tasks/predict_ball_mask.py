3cf2dcf0a83d9546208b92a288071817719c772f
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-14 16:30:47 +0200

    Working on local machine as well

--- detector/tasks/predict_ball_mask.py
+++ detector/tasks/predict_ball_mask.py
@@ -1,11 +1,9 @@
 import os
 import pathlib
 
-import onnx
 from vidgear.gears import CamGear
 
 import cv2 as cv
-import onnxruntime as ort
 import numpy as np
 import fastdeploy as fd
 
@@ -17,16 +15,19 @@ if __name__ == '__main__':
     print('Building model...')
     option = fd.RuntimeOption()
     option.use_gpu()
-    model_file = os.path.join('/home', 'ubuntu', 'PaddleSeg', 'output', 'inference_model', 'model.pdmodel')
-    params_file = os.path.join('/home', 'ubuntu', 'PaddleSeg', 'output', 'inference_model', 'model.pdiparams')
-    config_file = os.path.join('/home', 'ubuntu', 'PaddleSeg', 'output', 'inference_model', 'deploy.yaml')
+    # model_file = os.path.join('/home', 'ubuntu', 'PaddleSeg', 'output', 'inference_model', 'model.pdmodel')
+    # params_file = os.path.join('/home', 'ubuntu', 'PaddleSeg', 'output', 'inference_model', 'model.pdiparams')
+    # config_file = os.path.join('/home', 'ubuntu', 'PaddleSeg', 'output', 'inference_model', 'deploy.yaml')
+    model_file = pathlib.Path('/home/peiva/PycharmProjects/PaddleSeg/output/inference_model/model.pdmodel')
+    params_file = pathlib.Path('/home/peiva/PycharmProjects/PaddleSeg/output/inference_model/model.pdiparams')
+    config_file = pathlib.Path('/home/peiva/PycharmProjects/PaddleSeg/output/inference_model/deploy.yaml')
     model = fd.vision.segmentation.PaddleSegModel(
-        model_file, params_file, config_file, runtime_option=option
+        str(model_file), str(params_file), str(config_file), runtime_option=option
     )
     print('Reading video...')
     stream = CamGear(
-        #source='/mnt/DATA/tesi/dataset/dataset_youtube/pallacanestro_trieste/stagione_2019-20_legabasket/pallacanestro_trieste-dolomiti_energia_trentino/final_cut.mp4'
-        source='/home/ubuntu/test_video.mp4'
+        source='/mnt/DATA/tesi/dataset/dataset_youtube/pallacanestro_trieste/stagione_2019-20_legabasket/pallacanestro_trieste-dolomiti_energia_trentino/final_cut.mp4'
+        #source='/home/ubuntu/test_video.mp4'
     ).start()
     index = 1
     while True:
@@ -34,13 +35,12 @@ if __name__ == '__main__':
         if frame is None:
             break
         frame_resized = cv.resize(frame, (2048, 1024))
-        #cv.imshow('Output', frame_with_overlay)
-        #cv.imwrite(f'/home/peiva/experiments/test_videos/overlay{index}.png', frame_with_overlay)
         print(f'Predicting frame {index}...')
         result = model.predict(frame_resized)
         vis_im = fd.vision.vis_segmentation(frame_resized, result, weight=0.5)
-        print(f'Writing overlay {index} to disk...')
-        cv.imwrite(f'/home/ubuntu/results/overlay{index}.png', vis_im)
+        # print(f'Writing overlay {index} to disk...')
+        # cv.imwrite(f'/home/ubuntu/results/overlay{index}.png', vis_im)
+        cv.imshow('Output', vis_im)
         index += 1
 
         key = cv.waitKey(1) & 0xFF
