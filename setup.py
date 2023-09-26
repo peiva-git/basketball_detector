b293192cf8b38dd3aa7068bd80ad7ccaf07106d1
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-09-26 19:02:40 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ dependencies = [
     'opencv-python==4.8.*',
     'vidgear==0.3.*',
     'statistics',
-    'fastdeploy-gpu-python==1.0.*',
+    'fastdeploy-gpu-python',
     'scikit-image==0.21.*',
     'pdoc'
 ]
