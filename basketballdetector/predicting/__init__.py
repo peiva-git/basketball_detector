b7b9c134f4da93d6d9f09dc833bb11477829bfc8
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-11-20 22:34:44 +0100

    Refactor

--- basketballdetector/predicting/__init__.py
+++ basketballdetector/predicting/__init__.py
@@ -6,6 +6,4 @@ simply produce an output from a single image, the `basketballdetector.predicting
 class encompasses the whole prediction process starting from an input video.
 """
 
-from .predict_ball_mask import \
-    get_prediction_with_single_heatmap, get_prediction_with_multiple_heatmaps, \
-    PredictionHandler
+from .predict_ball_mask import PredictionHandler
