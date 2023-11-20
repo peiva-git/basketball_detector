a38faa486de7e555e5c6e094f9420248ca0d7368
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-11-20 21:58:03 +0100

    Removed multiple heatmaps code, output now rewriting previous line

--- basketballdetector/predicting/predict_ball_mask.py
+++ basketballdetector/predicting/predict_ball_mask.py
@@ -6,7 +6,6 @@ import pathlib
 import time
 import re
 
-from skimage import color
 from vidgear.gears import CamGear
 from vidgear.gears import WriteGear
 from statistics import mean
@@ -14,8 +13,6 @@ from statistics import mean
 import cv2 as cv
 import numpy as np
 import fastdeploy as fd
-
-from basketballdetector.predicting.utils import generate_random_crops
 
 
 def get_prediction_with_single_heatmap(
@@ -28,57 +25,8 @@ def get_prediction_with_single_heatmap(
     :param paddle_seg_model: The model performing the prediction
     :return: The predicted mask, overlay on the input image
     """
-    result = paddle_seg_model.predict(image)
-    segmented_image = fd.vision.vis_segmentation(image, result, weight=0.5)
+
     return segmented_image
-
-
-def get_prediction_with_multiple_heatmaps(
-        image: np.ndarray,
-        paddle_seg_model: fd.vision.segmentation.PaddleSegModel,
-        number_of_crops: int,
-        variance: int) -> np.ndarray:
-    """
-    This function generates a mask prediction from an input image and multiple random crops of that same image,
-    using stacked heatmaps. It is therefore more accurate but slower.
-    :param image: The input image
-    :param paddle_seg_model: The model performing the prediction
-    :param number_of_crops: Number of random crops to consider for the prediction
-    :param variance: How much the random crops should differ from the first one.
-    See `basketballdetector.predicting.utils.generate_random_crops`
-    :return: The predicted mask, overlay on the input image
-    """
-    random_crops = generate_random_crops(image, number_of_crops, variance)
-    # results = [
-    #     paddle_seg_model.predict(
-    #         np.pad(
-    #             crop[2],
-    #             (
-    #                 (crop[1], image.shape[0] - (crop[1] + crop[2].shape[0])),
-    #                 (crop[0], image.shape[1] - (crop[0] + crop[2].shape[1])),
-    #                 (0, 0)
-    #             ),
-    #             constant_values=127.5
-    #         )
-    #     )
-    #     for crop in random_crops
-    # ]
-    results = paddle_seg_model.batch_predict([
-        np.pad(
-            crop[2],
-            (
-                (crop[1], image.shape[0] - (crop[1] + crop[2].shape[0])),
-                (crop[0], image.shape[1] - (crop[0] + crop[2].shape[1])),
-                (0, 0)
-            ),
-            constant_values=127.5
-        )
-        for crop in random_crops
-    ])
-    averaged_heatmap = np.mean([np.reshape(result.label_map, result.shape) for result in results], axis=0)
-    rescaled_heatmap = averaged_heatmap * 255
-    rescaled_heatmap = rescaled_heatmap.astype(np.uint8)
-    return color.label2rgb(rescaled_heatmap, image, saturation=1, alpha=0.5)
 
 
 class PredictionHandler:
@@ -93,12 +41,10 @@ class PredictionHandler:
                  params_file_path: str,
                  config_file_path: str,
                  input_video_path: str,
-                 stack_heatmaps: bool = False,
                  use_trt: bool = False):
         self.__model_file = pathlib.Path(model_file_path)
         self.__params_file = pathlib.Path(params_file_path)
         self.__config_file = pathlib.Path(config_file_path)
-        self.__stack_heatmaps = stack_heatmaps
         self.__use_trt = use_trt
         if re.match(self.__YT_URL_REGEX, input_video_path) is not None:
             # the input video path is a YouTube link
@@ -118,17 +64,7 @@ class PredictionHandler:
         model = fd.vision.segmentation.PaddleSegModel(
             str(self.__model_file), str(self.__params_file), str(self.__config_file), runtime_option=option
         )
-        if self.__stack_heatmaps:
-            model.postprocessor.apply_softmax = True
         return model
-
-    @property
-    def number_of_crops(self):
-        return self.__number_of_crops
-
-    @number_of_crops.setter
-    def number_of_crops(self, number_of_crops: int):
-        self.__number_of_crops = number_of_crops
 
     @property
     def predictions_target_directory(self):
@@ -187,21 +123,17 @@ class PredictionHandler:
         writer.close()
 
     def __obtain_prediction(self, frame):
-        frame_resized = cv.resize(frame, (2048, 1024))
-        print(f'Predicting frame {self.__counter}...')
         start = time.time()
-        if self.__stack_heatmaps:
-            output = get_prediction_with_multiple_heatmaps(
-                frame_resized, self.__model, self.__number_of_crops, variance=100
-            )
-        else:
-            output = get_prediction_with_single_heatmap(frame_resized, self.__model)
+        frame_resized = cv.resize(frame, (2048, 1024))
+        result = self.__model.predict(frame_resized)
+        segmented_image = fd.vision.vis_segmentation(frame_resized, result, weight=0.5)
         end = time.time()
-        print(f'Took {end - start} seconds to process frame {self.__counter}')
         self.__frame_processing_times.append(end - start)
-        print(f'Average processing speed: {mean(self.__frame_processing_times)} seconds')
-        print(f'{1 / (end - start)} FPS')
-        return output
+        print(
+            f'Average processing speed: {mean(self.__frame_processing_times)} seconds, {1 / (end - start)} FPS',
+            end='\r'
+        )
+        return segmented_image
 
 
 if __name__ == '__main__':
